4dc4865502383c3424f95987f7af7d7b82669af0
VarEn1k <kola201510@gmail.com>
2020-11-14 18:02:51 +0200

    Firs version

---
+++ src/Anagram.java
@@ -0,0 +1,19 @@
+public class Anagram {
+    String value;
+
+    Anagram(String value) {
+        this.value = value;
+    }
+
+    public String solve() {
+        String[] letters = new String[value.length()];
+        for (int i = 0; i < value.length(); i++) {
+            letters[letters.length - i - 1] = value.substring(i, i + 1);
+        }
+        String solved = "";
+        for (int i = 0; i < value.length(); i++) {
+            solved += letters[i];
+        }
+        return solved;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        var task1 = "hsif";
+        var solution1 = new Anagram(task1) .solve();
+        System.out.println(task1+" -> " + solution1);
+    }
+}
